1aa8ae1292d0e0063a0cce82a185451a223c8199
Jéssica Beatriz <jessicabaf@hotmail.com>
2022-02-20 10:30:54 -0300

    Update another guess  the number.py
    update

--- Beginner Projects/guess the number/another guess  the number.py
+++ Beginner Projects/guess the number/another guess  the number.py
@@ -16,3 +16,16 @@ else:
 randon_number = random.randint(0, top_range)
 
 
+while True:
+    user_guess = input("Make a guess ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print("Please type a number next time: ")
+        continue
+    
+    if user_guess == randon_number:
+        print("You got it!")
+        break
+    else:
+        print("You got it wrong!")
