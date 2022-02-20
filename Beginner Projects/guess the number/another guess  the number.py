1dcaed3739781fa54f79d9013272870a58116b62
Jéssica Beatriz <jessicabaf@hotmail.com>
2022-02-20 10:50:15 -0300

    Update another guess  the number.py
    doing an elif instead of else - if - else

--- Beginner Projects/guess the number/another guess  the number.py
+++ Beginner Projects/guess the number/another guess  the number.py
@@ -14,10 +14,11 @@ else:
     quit()
     
 randon_number = random.randint(0, top_range)
-
+guesses = 0
 
 while True:
-    user_guess = input("Make a guess ")
+    guesses +=1
+    user_guess = input("Make a guess: ")
     if user_guess.isdigit():
         user_guess = int(user_guess)
     else:
@@ -27,5 +28,9 @@ while True:
     if user_guess == randon_number:
         print("You got it!")
         break
+    elif user_guess >randon_number:
+        print("You were above the number!")
     else:
-        print("You got it wrong!")
+        print("You were below the number!")
+            
+print("You got it in",guesses, "guesses!")
